dacf84d021668ebfedb7da231c884d38ae1535de
Chris Goodin <cgoodin@cavs.msstate.edu>
2022-10-04 16:44:07 -0500

    Code cleanup

--- include/mavs-ros2/mavs_camera_node.h
+++ include/mavs-ros2/mavs_camera_node.h
@@ -16,22 +16,25 @@
 class MavsCameraNode : public MavsSensorNode{
   public:
 	MavsCameraNode(): MavsSensorNode(){
-		//std::cout<<"Creating camera node "<<std::endl;
-		cam = NULL;
+		cam_ = NULL;
+
 		LoadCameraParams();
 
-		camera_pub = this->create_publisher<sensor_msgs::msg::Image>("camera", 10);
+		camera_pub_ = this->create_publisher<sensor_msgs::msg::Image>("camera", 10);
 
-		timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/update_rate_hz)),std::bind(&MavsCameraNode::TimerCallback, this));
+		timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/update_rate_hz_)),std::bind(&MavsCameraNode::TimerCallback, this));
 	}
 
-
 	~MavsCameraNode(){
-		if (cam) delete cam;
+		if (cam_) delete cam_;
 	}
 
   private:
+	// class member data
+	mavs::sensor::camera::Camera *cam_;
+	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_pub_;
 
+	// class member functions
 	void LoadCameraParams(){
 		std::string camera_type = GetStringParam("camera_type", "rgb");
 		int nx = GetIntParam("num_horizontal_pix", 256);
@@ -42,50 +45,46 @@ class MavsCameraNode : public MavsSensorNode{
 		bool render_shadows = GetBoolParam("render_shadows", true);
 		
 		if (camera_type == "rgb"){
-			cam = new mavs::sensor::camera::RgbCamera;
+			cam_ = new mavs::sensor::camera::RgbCamera;
 		}
 		else if (camera_type == "rccb"){
-			cam = new mavs::sensor::camera::RccbCamera;
+			cam_ = new mavs::sensor::camera::RccbCamera;
 		}
 		else if (camera_type == "fisheye"){
-			cam = new mavs::sensor::camera::RgbCamera;
+			cam_ = new mavs::sensor::camera::RgbCamera;
 		}
 		else if (camera_type == "nir"){
-			cam = new mavs::sensor::camera::RgbCamera;	
+			cam_ = new mavs::sensor::camera::RgbCamera;	
 		}
 		else if (camera_type == "lwir"){
-			cam = new mavs::sensor::camera::RgbCamera;
+			cam_ = new mavs::sensor::camera::RgbCamera;
 		}
 		else {
 			std::cerr<<"WARNING: CAMERA TYPE "<<camera_type<<" NOT RECOGNIZED, USING RGB"<<std::endl;
-			cam = new mavs::sensor::camera::RgbCamera;
+			cam_ = new mavs::sensor::camera::RgbCamera;
 		}
-		cam->Initialize(nx,ny,px,py,flen);
-		cam->SetRelativePose(glm::vec3(offset[0], offset[1], offset[2]), glm::quat(relor[0], relor[1], relor[2], relor[3]));
-		cam->SetRenderShadows(render_shadows);
+		cam_->Initialize(nx,ny,px,py,flen);
+		cam_->SetRelativePose(glm::vec3(offset_[0], offset_[1], offset_[2]), glm::quat(relor_[0], relor_[1], relor_[2], relor_[3]));
+		cam_->SetRenderShadows(render_shadows);
 	}
 
-	mavs::sensor::camera::Camera *cam;
-
-	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr camera_pub; // = n->create_publisher<sensor_msgs::msg::Image>("camera", 10);
-
 	void TimerCallback(){
-        //RCLCPP_INFO(this->get_logger(), "MAVS Camera class callback");
+
 		MavsSensorNode::TimerCallback();
 
 		glm::vec3 pos(pose_.pose.pose.position.x, pose_.pose.pose.position.y, pose_.pose.pose.position.z);
 		glm::quat ori(pose_.pose.pose.orientation.w, pose_.pose.pose.orientation.x, pose_.pose.pose.orientation.y, pose_.pose.pose.orientation.z);
 
-		cam->SetPose(pos, ori);
+		cam_->SetPose(pos, ori);
 
-		cam->Update(&env, dt);
+		cam_->Update(&env_, dt_);
 		
-		if (display)cam->Display();
+		if (display_)cam_->Display();
 
 		sensor_msgs::msg::Image img;
-		mavs::Image mavs_img = cam->GetRosImage();
+		mavs::Image mavs_img = cam_->GetRosImage();
 		mavs_ros_utils::CopyFromMavsImage(img, mavs_img);
-		camera_pub->publish(img);
+		camera_pub_->publish(img);
     }
 
 };
--- include/mavs-ros2/mavs_sensor_node.h
+++ include/mavs-ros2/mavs_sensor_node.h
@@ -14,62 +14,54 @@
 class MavsSensorNode : public MavsNode {
   public:
 	MavsSensorNode(): MavsNode(){
-		//std::cout<<"Creating base sensor node "<<std::endl;
 		odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("odometry_true", 10, std::bind(&MavsSensorNode::OdomCallback, this, std::placeholders::_1));
 		anim_sub = this->create_subscription<geometry_msgs::msg::PoseArray>("all_poses_pub", 10, std::bind(&MavsSensorNode::PosesCallback, this, std::placeholders::_1));
 		
-		update_rate_hz = 10.0f;
+		update_rate_hz_ = 10.0f;
+
 		LoadParams();
 
-		LoadMavsData();
-
-		timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/update_rate_hz)),std::bind(&MavsSensorNode::TimerCallback, this));
+		timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0/update_rate_hz_)),std::bind(&MavsSensorNode::TimerCallback, this));
 	}
 
   protected:
-	void LoadMavsData(){
+  	// class member data
+  	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+  	rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr anim_sub;
+	rclcpp::TimerBase::SharedPtr timer_;
+	nav_msgs::msg::Odometry pose_;
+	geometry_msgs::msg::PoseArray anim_poses_;
+	mavs::environment::Environment env_;
+	mavs::raytracer::embree::EmbreeTracer scene_;
+	std::vector<float> offset_;
+	std::vector<float> relor_;
+	bool display_;
+	float update_rate_hz_;
+	float dt_;
+
+	void LoadParams(){
+		std::string scene_file = GetStringParam( "scene_file", "cube_scene.json");
+		offset_ = GetFloatArrayParam("offset",std::vector<float>(0));
+		relor_ = GetFloatArrayParam("orientation",std::vector<float>(0));
+		display_ = GetBoolParam("display", true);
+		update_rate_hz_ = GetFloatParam("update_rate_hz", 10.0f);
+		std::vector<std::string> vehicle_files = GetStringArrayParam("vehicle_files", std::vector<std::string>(0));
+		dt_ = 1.0f/update_rate_hz_;
+
 		mavs::MavsDataPath mdp;
 		std::string mavs_data_path = mdp.GetPath();
-		scene.Load(mavs_data_path+"/scenes/"+scene_file);
-		scene.TurnOffLabeling();
-		env.SetRaytracer(&scene);
+		scene_.Load(mavs_data_path+"/scenes/"+scene_file);
+		scene_.TurnOffLabeling();
+		env_.SetRaytracer(&scene_);
 
 		for (int nv =0; nv<(int)vehicle_files.size();nv++){
 			mavs::vehicle::Rp3dVehicle mavs_veh;
 			mavs_veh.Load(mavs_data_path+"/vehicles/rp3d_vehicles/"+vehicle_files[nv]);
 			mavs_veh.SetPosition(-10000.0f, -10000.0f, -10000.0f);
 			mavs_veh.SetOrientation(1.0f, 0.0f, 0.0f, 0.0f);
-			mavs_veh.Update(&env, 0.0, 0.0, 0.0, 0.00001);
+			mavs_veh.Update(&env_, 0.0, 0.0, 0.0, 0.00001);
 		}
 	}
-
-	void LoadParams(){
-		scene_file = GetStringParam( "scene_file", "cube_scene.json");
-		offset = GetFloatArrayParam("offset",std::vector<float>(0));
-		relor = GetFloatArrayParam("orientation",std::vector<float>(0));
-		display = GetBoolParam("display", true);
-		update_rate_hz = GetFloatParam("update_rate_hz", 10.0f);
-		vehicle_files = GetStringArrayParam("vehicle_files", std::vector<std::string>(0));
-		dt = 1.0f/update_rate_hz;
-	}
-
-  	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
-  	rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr anim_sub;
-
-	rclcpp::TimerBase::SharedPtr timer_;
-
-	nav_msgs::msg::Odometry pose_;
-	geometry_msgs::msg::PoseArray anim_poses_;
-
-	mavs::environment::Environment env;
-	mavs::raytracer::embree::EmbreeTracer scene;
-	std::string scene_file;
-	std::vector<float> offset;
-	std::vector<float> relor;
-	bool display;
-	float update_rate_hz;
-	float dt;
-	std::vector<std::string> vehicle_files;
 
 	void OdomCallback(const nav_msgs::msg::Odometry::SharedPtr rcv_msg){
 		pose_ = *rcv_msg;
@@ -80,12 +72,11 @@ class MavsSensorNode : public MavsNode {
 	}
 
 	void TimerCallback(){
-        //RCLCPP_INFO(this->get_logger(), "MAVS Sensor base class callback");
-		if (env.GetNumberOfActors()>=(int)(anim_poses_.poses.size())){
+		if (env_.GetNumberOfActors()>=(int)(anim_poses_.poses.size())){
 			for (int i=0;i<(int)anim_poses_.poses.size();i++){
 				glm::vec3 tpos(anim_poses_.poses[i].position.x, anim_poses_.poses[i].position.y, anim_poses_.poses[i].position.z);
 				glm::quat tori(anim_poses_.poses[i].orientation.w, anim_poses_.poses[i].orientation.x, anim_poses_.poses[i].orientation.y, anim_poses_.poses[i].orientation.z);
-				env.SetActorPosition(i, tpos, tori, dt, true);
+				env_.SetActorPosition(i, tpos, tori, dt_, true);
 			}
 		}
     }
