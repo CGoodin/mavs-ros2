b7d38b41bb9f0e2b3f913caaec4460d7de5fc5e3
Chris Goodin <cgoodin@cavs.msstate.edu>
2025-08-07 11:53:24 -0500

    Updated the scene creator node to run a single sim at a time

--- include/mavs-ros2/mavs_vehicle_node_scene_creator.h
+++ include/mavs-ros2/mavs_vehicle_node_scene_creator.h
@@ -1,89 +1,186 @@
-#ifndef MAVS_VEHICLE_NODE_SCENE_CREATOR_H
-#define MAVS_VEHICLE_NODE_SCENE_CREATOR_H
-
 // package includes
 #include "mavs-ros2/mavs_ros_utils.h"
 #include "mavs-ros2/mavs_sensor_node.h"
-
 // ros includes
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
 #include "rosgraph_msgs/msg/clock.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-
 // mavs includes
 #include "mavs_core/data_path.h"
 #include "vehicles/rp3d_veh/mavs_rp3d_veh.h"
 #include "raytracers/embree_tracer/embree_tracer.h"
 #include "sensors/mavs_sensors.h"
-#include "sensors/camera/rgb_camera.h"
 #include "mavs_core/terrain_generator/terrain_elevation_functions.h"
-
-#include <string>
-#include <vector>
 
 class MavsVehicleNodeSceneCreator : public MavsNode
 {
 public:
-    MavsVehicleNodeSceneCreator();
+	MavsVehicleNodeSceneCreator() : MavsNode(){
+		dt_ = 0.01;
+		nsteps_ = 0;
+		elapsed_time_ = 0.0f;
+
+		twist_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 1, std::bind(&MavsVehicleNodeSceneCreator::TwistCallback, this, std::placeholders::_1));
+		odom_true_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("odometry_true", 10);
+		anim_poses_pub_ = this->create_publisher<geometry_msgs::msg::PoseArray>("anim_poses", 10);
+
+		LoadVehicleParams();
+
+		timer_ = this->create_wall_timer(std::chrono::milliseconds((int)(1000.0 / (1.0/dt_))), std::bind(&MavsVehicleNodeSceneCreator::TimerCallback, this));
+		render_steps_ = std::max(1,(int)(0.1f/dt_));
+	}
+
+	~MavsVehicleNodeSceneCreator(){}
 
 private:
-    // ROS interfaces
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_true_pub_;
-    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr anim_poses_pub_;
-    rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
+	rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_true_pub_;
+	rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr anim_poses_pub_;
+	rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_pub_;
+	
+	float throttle_;
+	float steering_;
+	float braking_;
+	rclcpp::TimerBase::SharedPtr timer_;
+	mavs::environment::Environment env_;
+	mavs::raytracer::embree::EmbreeTracer *scene_ptr_;
+	mavs::terraingen::TerrainCreator terrain_creator_;
+	bool render_debug_;
+	bool use_human_driver_;
+	bool use_sim_time_;
+	mavs::sensor::camera::RgbCamera camera_;
+	mavs::vehicle::Rp3dVehicle mavs_veh_;
+	double dt_;
+	int nsteps_;
+	int render_steps_;
+	float elapsed_time_;
 
-    // Simulation parameters and state
-    float dt_;
-    int nsteps_;
-    int render_steps_;
-    float elapsed_time_;
-    float throttle_, steering_, braking_;
-    bool render_debug_;
-    bool use_human_driver_;
-    bool use_sim_time_;
+	void TwistCallback(const geometry_msgs::msg::Twist::SharedPtr rcv_msg){
+		throttle_ = rcv_msg->linear.x;
+		braking_ = rcv_msg->linear.y;
+		steering_ = rcv_msg->angular.z;
+	}
 
-    // Scene creator specific parameters
-    std::string mode_;             // "manual" or "automated"
-    int iterations_;
-    float ditch_depth_start_;
-    float ditch_width_start_;
-    std::string results_path_;
+	void LoadVehicleParams(){
+		// determine if sim time will be used
+		use_sim_time_ = this->get_parameter("use_sim_time").as_bool();
+		
+		if (use_sim_time_){
+			clock_pub_ = this->create_publisher<rosgraph_msgs::msg::Clock>("clock", 1);
+		}
 
-    std::vector<float> ditch_depths_;
-    std::vector<float> top_widths_;
-    std::vector<float> selected_depths_;
-    std::vector<float> selected_widths_;
+		// load parameters
+		std::string scene_file = GetStringParam("scene_file", "cube_scene.json");
+		std::string rp3d_vehicle_file = GetStringParam("rp3d_vehicle_file", "l200.json");
+		float soil_strength = GetFloatParam("soil_strength", 250.0f);
+		std::string surface_type = GetStringParam("surface_type", "dry");
+		float x_init = GetFloatParam("Initial_X_Position", 0.0f);
+		float y_init = GetFloatParam("Initial_Y_Position", 0.0f);
+		float heading_init = GetFloatParam("Initial_Heading", 0.0f);
+		render_debug_ = GetBoolParam("debug_camera", false);
+		use_human_driver_ = GetBoolParam("use_human_driver", false);
+		dt_ = GetFloatParam("dt",0.01f);
 
-    int max_iterations_;
-    int current_iteration_;
-    int depth_index_;
-    int width_index_;
+		glm::vec3 initial_position(x_init, y_init, 1.0f);
+		glm::quat initial_orientation(cos(0.5 * heading_init), 0.0f, 0.0f, sin(0.5 * heading_init));
 
-    // MAVS objects
-    mavs::environment::Environment env_;
-    mavs::raytracer::embree::EmbreeTracer scene_;
-    mavs::sensor::camera::RgbCamera camera_;
-    mavs::vehicle::Rp3dVehicle mavs_veh_;
+		mavs::MavsDataPath mdp;
+		std::string mavs_data_path = mdp.GetPath();
+		
+		terrain_creator_.AddTrapezoid(6.0f, 12.0f, 2.0f, 20.0f);
+		terrain_creator_.AddTrapezoid(6.0f, 12.0f, -2.0f, 30.0f);
+		terrain_creator_.CreateTerrain(-25.0f, -25.0f, 200.0f, 25.0f, 0.5f);
+		scene_ptr_ = terrain_creator_.GetScenePointer();
+		scene_ptr_->TurnOffLabeling();
 
-    // Member functions
-    void TwistCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
+		env_.SetRaytracer(scene_ptr_);
+		env_.SetGlobalSurfaceProperties(surface_type, 6894.76f * soil_strength);
 
-    void PrepareSimulationParams();
-    std::vector<float> GenerateRange(float start, float end, float step);
+		mavs_veh_.Load(mavs_data_path + "/vehicles/rp3d_vehicles/" + rp3d_vehicle_file);
+		mavs_veh_.SetPosition(initial_position.x, initial_position.y, initial_position.z);
+		mavs_veh_.SetOrientation(initial_orientation.w, initial_orientation.x, initial_orientation.y, initial_orientation.z);
+		mavs_veh_.Update(&env_, throttle_, steering_, braking_, 0.00001);
 
-    void TimerCallback();
-    void SetupTerrain(float top_width, float base_width, float depth);
-    void ResetVehicleState();
-    std::vector<float> RunSimulation();
-    void LogResults(float ditch_depth, float ditch_width, const std::vector<float>& speeds);
+		camera_.Initialize(640, 480, 0.0046667, 0.0035, 0.0035);
+		camera_.SetRenderShadows(true);
+		camera_.SetRelativePose(glm::vec3(-9.0, 0.0, 2.5), glm::quat(1.0f, 0.0f, 0.0f, 0.0f));
+	}
 
-    void LoadVehicleAndScene();
-    void UpdateHumanDrivingCommands();
+	void UpdateHumanDrivingCommands(){
+		throttle_ = 0.0;
+		steering_ = 0.0;
+		braking_ = 0.0;
+		std::vector<bool> driving_commands = camera_.GetKeyCommands();
+		if (driving_commands[0]){
+			throttle_ = 1.0;
+		}
+		else if (driving_commands[1]){
+			braking_ = -1.0;
+		}
+		if (driving_commands[2]){
+			steering_ = 1.0;
+		}
+		else if (driving_commands[3]){
+			steering_ = -1.0;
+		}
+	}
+
+	void TimerCallback(){
+		// vehicle state update
+		if (use_human_driver_) UpdateHumanDrivingCommands();
+
+		mavs_veh_.Update(&env_, throttle_, steering_, -braking_, dt_);
+		mavs::VehicleState veh_state = mavs_veh_.GetState();
+
+		nav_msgs::msg::Odometry true_odom = mavs_ros_utils::CopyFromMavsVehicleState(veh_state);
+
+		if (render_debug_ && nsteps_ % render_steps_ == 0){
+			camera_.SetPose(veh_state);
+			camera_.Update(&env_, 0.1);
+			camera_.Display();
+			nsteps_=0;
+		}
+
+		// publish the vehicle state as an odometry message
+		true_odom.header.stamp = this->now();
+		true_odom.header.frame_id = "odom";
+		odom_true_pub_->publish(true_odom);
+
+		// publish the tire poses as PoseArray Message
+		geometry_msgs::msg::PoseArray anim_poses;
+		geometry_msgs::msg::Pose vpose;
+		vpose.position = true_odom.pose.pose.position;
+		vpose.orientation = true_odom.pose.pose.orientation;
+		anim_poses.poses.push_back(vpose);
+		for (int i = 0; i < mavs_veh_.GetNumTires(); i++){
+			glm::vec3 tpos = mavs_veh_.GetTirePosition(i);
+			glm::quat tori = mavs_veh_.GetTireOrientation(i);
+			geometry_msgs::msg::Pose tpose;
+			tpose.position.x = tpos.x;
+			tpose.position.y = tpos.y;
+			tpose.position.z = tpos.z;
+			tpose.orientation.w = tori.w;
+			tpose.orientation.x = tori.x;
+			tpose.orientation.y = tori.y;
+			tpose.orientation.z = tori.z;
+			anim_poses.poses.push_back(tpose);
+		}
+		anim_poses.header.frame_id = this->get_namespace();
+		anim_poses.header.stamp = this->now();
+		anim_poses_pub_->publish(anim_poses);
+
+		// clock update
+		if (use_sim_time_){
+			int sec = (int)floor(elapsed_time_);
+			int nsec = (int)(1.0E9f * (elapsed_time_ - (float)sec));
+			rclcpp::Time tnow(sec, nsec);
+			rosgraph_msgs::msg::Clock cnow;
+			cnow.clock = tnow;
+			clock_pub_->publish(cnow);
+			elapsed_time_ += dt_;
+		}
+		nsteps_++;
+	}
 };
-
-#endif // MAVS_VEHICLE_NODE_SCENE_CREATOR_H
-
