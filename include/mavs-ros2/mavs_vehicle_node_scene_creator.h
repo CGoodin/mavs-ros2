678d732799f2648b28f5b2915c952e79522f18d3
JordonDJasper <149651948+JordonDJasper@users.noreply.github.com>
2025-11-07 16:20:16 -0600

    Update mavs_vehicle_node_scene_creator.h to remove logged results

--- include/mavs-ros2/mavs_vehicle_node_scene_creator.h
+++ include/mavs-ros2/mavs_vehicle_node_scene_creator.h
@@ -41,7 +41,7 @@ public:
 		render_steps_ = std::max(1,(int)(0.1f/dt_));
 	}
 
-	~MavsVehicleNodeSceneCreator() { LogResults(); }
+	//~MavsVehicleNodeSceneCreator() { LogResults(); }
 
 	void SetCurrentIteration(int curr_iter) { current_iteration_ = curr_iter; }
 private:
@@ -145,30 +145,30 @@ private:
 		}
 	}
 
-	void LogResults(){
-		float avg_speed = 0.0f;
-		if (!logged_speeds_.empty()) {
-			avg_speed = std::accumulate(logged_speeds_.begin(), logged_speeds_.end(), 0.0f) / logged_speeds_.size();
-		}
+	// void LogResults(){
+	// 	float avg_speed = 0.0f;
+	// 	if (!logged_speeds_.empty()) {
+	// 		avg_speed = std::accumulate(logged_speeds_.begin(), logged_speeds_.end(), 0.0f) / logged_speeds_.size();
+	// 	}
 
-		std::string filename = results_path_ + "/run_" + std::to_string(current_iteration_) + ".json";
+	// 	std::string filename = results_path_ + "/run_" + std::to_string(current_iteration_) + ".json";
 
-		std::ofstream file(filename);
-		if (file.is_open()) {
-			file << "{\n";
-			//file << "  \"iteration\": " << current_iteration_ << ",\n";
-			file << "  \"ditch_depth\": " << ditch_depth_ << ",\n";
-			file << "  \"ditch_width\": " << ditch_width_ << ",\n";
-			file << "  \"average_speed\": " << avg_speed << ",\n";
-			file << "  \"timestamp\": " << std::time(nullptr) << "\n";
-			file << "}\n";
+	// 	std::ofstream file(filename);
+	// 	if (file.is_open()) {
+	// 		file << "{\n";
+	// 		//file << "  \"iteration\": " << current_iteration_ << ",\n";
+	// 		file << "  \"ditch_depth\": " << ditch_depth_ << ",\n";
+	// 		file << "  \"ditch_width\": " << ditch_width_ << ",\n";
+	// 		file << "  \"average_speed\": " << avg_speed << ",\n";
+	// 		file << "  \"timestamp\": " << std::time(nullptr) << "\n";
+	// 		file << "}\n";
 
-			RCLCPP_INFO(this->get_logger(), "Saved results to %s", filename.c_str());
-		}
-		else {
-			RCLCPP_WARN(this->get_logger(), "Failed to open file for saving results: %s", filename.c_str());
-		}
-	}
+	// 		RCLCPP_INFO(this->get_logger(), "Saved results to %s", filename.c_str());
+	// 	}
+	// 	else {
+	// 		RCLCPP_WARN(this->get_logger(), "Failed to open file for saving results: %s", filename.c_str());
+	// 	}
+	// }
 
 	void UpdateHumanDrivingCommands(){
 		throttle_ = 0.0;
